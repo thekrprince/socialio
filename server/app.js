a34643a0d863e3a1bedf4faede14e9eca0017985
princekr96 <princekr700@gmail.com>
2023-03-08 00:00:28 +0530

    middleware added in app to parse the body

--- server/app.js
+++ server/app.js
@@ -9,14 +9,18 @@ mongoose.set('strictQuery', false);
 // Connect Database
 connectDB();
 
-const PORT = process.env.PORT || 4000;
+// Init middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define routes
 app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/users', require('./routes/api/users'));
 app.use('/api/post', require('./routes/api/post'));
 app.use('/api/comment', require('./routes/api/comment'));
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
