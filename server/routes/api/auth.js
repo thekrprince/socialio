0e1aa9ba47ea83882cd44535ccf8affc47153dc3
princekr96 <princekr700@gmail.com>
2023-03-14 15:22:15 +0530

    Auth middleware created

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-router.get('/', (req, res) => {
-  res.send('Login');
+const User = require('../../models/User');
+
+// @route     GET api/auth
+// @desc      Test route
+// @access    Public
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 router.post('/', (req, res) => {
