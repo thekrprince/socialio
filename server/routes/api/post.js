2acf713c0de1c70b7c972a85ad91303eed51e962
princekr96 <princekr700@gmail.com>
2023-03-24 22:42:06 +0530

    get post apis created

--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -38,4 +38,40 @@ router.post(
   }
 );
 
+// @route     GET api/post
+// @desc      Get all post
+// @access    Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route     GET api/post/:id
+// @desc      Get post by id
+// @access    Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id).sort({ date: -1 });
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
