3a49ef54340aa1247145ad8418b58b59ce76de64
Payal Sasmal <sasmalpayal@gmail.com>
2023-03-24 20:51:12 +0530

    registration css page started

--- client/src/components/authentication/Registration.js
+++ client/src/components/authentication/Registration.js
@@ -1,58 +1,62 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Regist from "./Registration.css";
 
-const baseURL= "http://localhost:4000/api/users";
+const baseURL = "http://localhost:4000/api/users";
 
 const Registration = () => {
-    // const [name, setName] = useState('');
-    // const [email, setEmail] = useState('');
-    // const [password, setPassword] = useState('');
-    const [inputChange, setInputChange] = useState({
-        name: '',
-        email: '',
-        password: '',
+  const [inputChange, setInputChange] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setInputChange({
+      ...inputChange,
+      [e.target.name]: e.target.value,
     });
-    const [registration, setRegistration] = useState(null);
+  };
 
-    // useEffect(() => {
-    //     axios.get(baseURL).then((res) => {
-    //         setRegistration(res.data);
-    //     });
-    // }, []);
-
-    const handleChange = (e) => {
-        setInputChange({
-            ...inputChange,
-            [e.target.name]: e.target.value,
-        });
-    };
-
-    console.log(inputChange);
-    const handleSubmit = event =>{
-        event.preventDefault();
-        axios.post(baseURL, {
-            name: inputChange.name,
-            email: inputChange.email,
-            password: inputChange.password,
-        }).then((res) => {
-            console.log("server response", res);
-        }).catch((err) => {
-            console.log("server response with error", err);
-        });
-        
-    };
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name" onChange={handleChange}/>
-            <label htmlFor="email">Email Id</label>
-            <input type="text" name="email" onChange={handleChange}/>
-            <label>Password</label>
-            <input type="password" name='password' onChange={handleChange}/>
-            <button>Submit</button>
-        </form>
-        
-    );
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post(baseURL, {
+        name: inputChange.name,
+        email: inputChange.email,
+        password: inputChange.password,
+      })
+      .then((res) => {
+        console.log("server response", res);
+      })
+      .catch((err) => {
+        console.log("server response with error", err);
+      });
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="container">
+        <div className="conatiner-name-top">
+          <h1 className="container-name">Register Here</h1>
+          <div className="div-inner-container">
+            <div className="input-div">
+              {/* <label htmlFor="name">Name</label> */}
+              <input type="text" name="name" onChange={handleChange} placeholder="Name"/>
+            </div>
+            <div className="input-div">
+              {/* <label htmlFor="email">Email Id</label> */}
+              <input type="text" name="email" onChange={handleChange} placeholder="Email" />
+            </div>
+            <div className="input-div">
+              {/* <label>Password</label> */}
+              <input type="password" name="password" onChange={handleChange} placeholder="Password"/>
+            </div>
+            <button className="button">Register</button>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default Registration;
