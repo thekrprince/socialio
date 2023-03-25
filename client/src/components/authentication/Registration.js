500a6bd2b8ccd1b5b19e928f2548f395c581d304
Payal Sasmal <sasmalpayal@gmail.com>
2023-03-25 20:02:05 +0530

    fixing css in registration page

--- client/src/components/authentication/Registration.js
+++ client/src/components/authentication/Registration.js
@@ -1,6 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import Regist from "./Registration.css";
+import "./Registration.css";
+import { FaAngleDoubleDown } from "react-icons/fa";
+// import { BsFillEmojiWinkFill, BsFillEmojiHeartEyesFill } from "react-icons/bs"
+// import { IoPeopleCircle } from "react-icons/io";
+import { SlPeople } from "react-icons/sl";
 
 const baseURL = "http://localhost:4000/api/users";
 
@@ -35,21 +39,41 @@ const Registration = () => {
   };
   return (
     <form onSubmit={handleSubmit}>
+      <div className="social-intro">
+        <span className="arrow-icon"><FaAngleDoubleDown /></span>
+        <h1>SOCIAL IO </h1>
+        <h2>Connect With New People <SlPeople/></h2>
+      </div>
       <div className="container">
         <div className="conatiner-name-top">
           <h1 className="container-name">Register Here</h1>
           <div className="div-inner-container">
             <div className="input-div">
               {/* <label htmlFor="name">Name</label> */}
-              <input type="text" name="name" onChange={handleChange} placeholder="Name"/>
+              <input
+                type="text"
+                name="name"
+                onChange={handleChange}
+                placeholder="Name"
+              />
             </div>
             <div className="input-div">
               {/* <label htmlFor="email">Email Id</label> */}
-              <input type="text" name="email" onChange={handleChange} placeholder="Email" />
+              <input
+                type="text"
+                name="email"
+                onChange={handleChange}
+                placeholder="Email"
+              />
             </div>
             <div className="input-div">
               {/* <label>Password</label> */}
-              <input type="password" name="password" onChange={handleChange} placeholder="Password"/>
+              <input
+                type="password"
+                name="password"
+                onChange={handleChange}
+                placeholder="Password"
+              />
             </div>
             <button className="button">Register</button>
           </div>
