d20da848e76081b624642f3ba8f2d3009fe4d858
Payal Sasmal <sasmalpayal@gmail.com>
2023-03-27 23:23:12 +0530

    clearning field is completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Registration from './components/authentication/Registration';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
       <Registration/>
+      {/* <ToastContainer/> */}
     </div>
   );
 }
--- client/src/components/authentication/Registration.js
+++ client/src/components/authentication/Registration.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
 import "./Registration.css";
+import "react-toastify/dist/ReactToastify.css";
 import { FaAngleDoubleDown } from "react-icons/fa";
 import { SlPeople } from "react-icons/sl";
 
@@ -12,6 +14,7 @@ const Registration = () => {
     email: "",
     password: "",
   });
+  const [successfullyRegistered, setSuccessfullyRegistered] = useState(false);
 
   const handleChange = (e) => {
     setInputChange({
@@ -22,6 +25,7 @@ const Registration = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
     axios
       .post(baseURL, {
         name: inputChange.name,
@@ -30,59 +34,69 @@ const Registration = () => {
       })
       .then((res) => {
         console.log("server response", res);
+        toast.success("You have Sucessfully Registered with SocialIO 💐!");
+        event.target.reset();
       })
       .catch((err) => {
         console.log("server response with error", err);
       });
   };
-  return (
-    <div className="registration-container">
-      <div className="social-intro">
-        <span className="arrow-icon">
-          <FaAngleDoubleDown size={70} />
-        </span>
-        <h1 className="heading">SOCIAL IO </h1>
-        <h4 className="heading-slogan">
-          Connect With New People <SlPeople />
-        </h4>
-        <button className="aboutUsButton">About US</button>
-      </div>
 
-      <form onSubmit={handleSubmit}>
-        <div className="container">
-          <div className="conatiner-name-top">
-            <h1 className="container-name">Register Here</h1>
-            <div className="div-inner-container">
-              <div className="input-div">
-                <input
-                  type="text"
-                  name="name"
-                  onChange={handleChange}
-                  placeholder="Name"
-                />
+  return (
+    <>
+      <div className="registration-container">
+        <div className="social-intro">
+          <span className="arrow-icon">
+            <FaAngleDoubleDown size={70} />
+          </span>
+          <h1 className="heading">SOCIAL IO </h1>
+          <h4 className="heading-slogan">
+            Connect With New People <SlPeople />
+          </h4>
+          <button className="aboutUsButton">About US</button>
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <div className="container">
+            <div className="conatiner-name-top">
+              <h1 className="container-name">Register Here</h1>
+              <div className="div-inner-container">
+                <div className="input-div">
+                  <input
+                    type="text"
+                    name="name"
+                    onChange={handleChange}
+                    placeholder="Name"
+                  />
+                </div>
+                <div className="input-div">
+                  <input
+                    type="text"
+                    name="email"
+                    onChange={handleChange}
+                    placeholder="Email"
+                  />
+                </div>
+                <div className="input-div">
+                  <input
+                    type="password"
+                    name="password"
+                    onChange={handleChange}
+                    placeholder="Password"
+                  />
+                </div>
+                <button className="button">Register</button>
               </div>
-              <div className="input-div">
-                <input
-                  type="text"
-                  name="email"
-                  onChange={handleChange}
-                  placeholder="Email"
-                />
-              </div>
-              <div className="input-div">
-                <input
-                  type="password"
-                  name="password"
-                  onChange={handleChange}
-                  placeholder="Password"
-                />
-              </div>
-              <button className="button">Register</button>
             </div>
           </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+      {/* <p>
+        Already Have an account?
+        <span className="span-switch">Sign In</span>
+      </p> */}
+      <ToastContainer />
+    </>
   );
 };
 
