acd0c1685c894ee4eeacb804a6468502e275a805
Payal Sasmal <sasmalpayal@gmail.com>
2023-04-02 17:35:49 +0530

    removed unnecessary package.json files

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,17 @@
 import './App.css';
-import { RouterProvider, createHashRouter, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Login from './components/authentication/Login';
 import Registration from './components/authentication/Registration';
 
-
-
+const router = createBrowserRouter([
+  {path: "/", element: <Registration/>},
+  {path: "/login", element: <Login/>},
+  ]);
 
 function App() {
-
-  const router = createBrowserRouter([
-    {
-
-      path: "/",
-      element: (
-        <Login/>
-      )
-    },
-    {
-        path: "/",
-        element: (
-          <Login/>
-        )
-    }
-    ],
-  ) ;
-
   return (
     <div className="App">
       <RouterProvider router={router}/>
-      
     </div>
   );
 }
