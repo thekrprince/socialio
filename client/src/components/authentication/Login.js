ffb6a81c6633ca420211182336f9f7e729cae7df
Payal Sasmal <sasmalpayal@gmail.com>
2023-04-03 23:15:42 +0530

    autheticating using login

--- client/src/components/authentication/Login.js
+++ client/src/components/authentication/Login.js
@@ -1,62 +1,84 @@
-import { Link } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./Registration.scss";
 import { FaAngleDoubleDown } from "react-icons/fa";
 import { SlPeople } from "react-icons/sl";
+import { useState } from "react";
+import axios from "axios";
+
+const baseURL = "http://localhost:4000/api/auth";
+
 const Login = () => {
+  const [fetchInputChangeValue, setFetchInputChangeValue] = useState({
+    email: "",
+    password: "",
+  });
 
-    const inputChangeHandler = () => {
-      
-    }
+  const fetchChangesOnFields = (event) => {
+    setFetchInputChangeValue({
+      ...fetchInputChangeValue,
+      [event.target.name]: [event.target.value],
+    });
+  };
+  const inputChangeHandler = (e) => {
+    e.preventDefault();
+    axios.post(baseURL, {
+      email: fetchInputChangeValue.email,
+      password: fetchInputChangeValue.password,
+    }).then((res) => {
+      console.log("sucessfully loggedin", res);
+    });
+  };
 
-    return (
-      <>
-        <div className="registration-container">
-          <div className="social-intro">
-            <span className="arrow-icon">
-              <FaAngleDoubleDown size={70} />
-            </span>
-            <h1 className="heading">SOCIAL IO </h1>
-            <h4 className="heading-slogan">
-              Connect With New People <SlPeople />
-            </h4>
-            <button className="aboutUsButton">About US</button>
-          </div>
-  
-          <form onSubmit={inputChangeHandler}>
-            <div className="container">
-              <div className="conatiner-name-top">
-                <h1 className="container-name">Login Here</h1>
-                <div className="div-inner-container">
-                  <div className="input-div">
-                    <input
-                      type="text"
-                      name="email"
-                      onChange={inputChangeHandler}
-                      placeholder="Email"
-                    />
-                  </div>
-                  <div className="input-div">
-                    <input
-                      type="password"
-                      name="password"
-                      onChange={inputChangeHandler}
-                      placeholder="Password"
-                    />
-                  </div>
-                  <button className="button">Login</button>
-                  <p>Don't have an account??&nbsp;
-                    <Link to="/registration"> Register now</Link></p>
+  return (
+    <>
+      <div className="registration-container">
+        <div className="social-intro">
+          <span className="arrow-icon">
+            <FaAngleDoubleDown size={70} />
+          </span>
+          <h1 className="heading">SOCIAL IO </h1>
+          <h4 className="heading-slogan">
+            Connect With New People <SlPeople />
+          </h4>
+          <button className="aboutUsButton">About US</button>
+        </div>
+
+        <form onSubmit={inputChangeHandler}>
+          <div className="container">
+            <div className="conatiner-name-top">
+              <h1 className="container-name">Login Here</h1>
+              <div className="div-inner-container">
+                <div className="input-div">
+                  <input
+                    type="text"
+                    name="email"
+                    onChange={fetchChangesOnFields}
+                    placeholder="Email"
+                  />
                 </div>
+                <div className="input-div">
+                  <input
+                    type="password"
+                    name="password"
+                    onChange={fetchChangesOnFields}
+                    placeholder="Password"
+                  />
+                </div>
+                <button className="button">Login</button>
+                <p>
+                  Don't have an account??&nbsp;
+                  <Link to="/registration"> Register now</Link>
+                </p>
               </div>
             </div>
-          </form>
-        </div>
-        <ToastContainer />
-        </>
-    );
-  };
-  
+          </div>
+        </form>
+      </div>
+      <ToastContainer />
+    </>
+  );
+};
 
-export default Login
+export default Login;
