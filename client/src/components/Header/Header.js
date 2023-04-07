fcaa642e18679d009a6b8e863868644c25aa92bc
Payal Sasmal <sasmalpayal@gmail.com>
2023-04-08 00:03:18 +0530

    search icon fixed

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,14 +1,18 @@
 import { BsSearch } from "react-icons/bs";
-import './Header.scss';
+import "./Header.scss";
+import icon_girl from "../../assets/icons-female-user.svg";
 
 const Header = () => {
   return (
     <div className="header_outer">
-        <input className="search" type="text" placeholder="Search..." />
-        <BsSearch className="icon"/>
-        <button className="button">Create Post</button>
-        <button></button>
-        
+      <input className="search" type="text" placeholder="Search..." />
+      <BsSearch className="search_icon" size={25} />
+      <div className="post_icon_manage">
+        <button className="create_post_button">Create Post</button>
+        <button className="profile_button">
+          <img src={icon_girl} alt="profile" className="profile_icon" />
+        </button>
+      </div>
     </div>
   );
 };
