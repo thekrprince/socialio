1329034838bf4055603c4286850aecd276760f52
Payal Sasmal <sasmalpayal@gmail.com>
2023-04-08 17:34:40 +0530

    created card for modal

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,22 +3,46 @@ import "./Header.scss";
 import icon_girl from "../../assets/icons-female-user.svg";
 import { useState } from "react";
 import CreatePostModal from "../Modal/CreatePostModal";
+import Card from "../UI/Card";
 
 const Header = () => {
-  
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="header_outer">
-      <input className="search" type="text" placeholder="Search..." />
-      <BsSearch className="search_icon" size={25} />
-      <div className="post_icon_manage">
-        <button className="create_post_button" onClick={() => setIsOpen(true)}>Create Post</button>
-        <button className="profile_button">
-          <img src={icon_girl} alt="profile" className="profile_icon" />
-        </button>
-        {isOpen && <CreatePostModal setIsOpen={setIsOpen}/>}
+    <>
+      <div className="header_outer">
+        <input className="search" type="text" placeholder="Search..." />
+        <BsSearch className="search_icon" size={25} />
+        <div className="post_icon_manage">
+          <button
+            className="create_post_button"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            Create Post
+          </button>
+          <button className="profile_button">
+            <img src={icon_girl} alt="profile" className="profile_icon" />
+          </button>
+        </div>
+        <Card>
+          <div className="portal">
+            {isOpen && (
+              <CreatePostModal
+                setIsOpen={setIsOpen}
+                onClose={() => setIsOpen(false)}
+              >
+                <div
+                  className="modal_portal"
+                  onClick={(e) => e.stopPropagation()}
+                />
+                <h1>Create Post</h1>
+                <input type="textarea" />
+                <button onClose={() => setIsOpen(false)}>Close</button>
+              </CreatePostModal>
+            )}
+          </div>
+        </Card>
       </div>
-    </div>
+    </>
   );
 };
 
