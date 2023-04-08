d91dc2b71773470e30d5ba6511161b2c7246e2fe
Payal Sasmal <sasmalpayal@gmail.com>
2023-04-08 12:11:07 +0530

    creating modal

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,19 +1,22 @@
 import { BsSearch } from "react-icons/bs";
 import "./Header.scss";
 import icon_girl from "../../assets/icons-female-user.svg";
+import { useState } from "react";
+import CreatePostModal from "../Modal/CreatePostModal";
 
 const Header = () => {
   
-
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="header_outer">
       <input className="search" type="text" placeholder="Search..." />
       <BsSearch className="search_icon" size={25} />
       <div className="post_icon_manage">
-        <button className="create_post_button">Create Post</button>
+        <button className="create_post_button" onClick={() => setIsOpen(true)}>Create Post</button>
         <button className="profile_button">
           <img src={icon_girl} alt="profile" className="profile_icon" />
         </button>
+        {isOpen && <CreatePostModal setIsOpen={setIsOpen}/>}
       </div>
     </div>
   );
---
+++ client/src/components/Modal/CreatePostModal.js
@@ -0,0 +1,14 @@
+
+const CreatePostModal = ({ setIsOpen }) => {
+    return (
+        <>
+        <div className="" onClick={() => setIsOpen(false)} />
+        <h1 >
+            create Post
+        </h1>
+        <input type="textarea" />
+        </>
+    );
+};
+
+export default CreatePostModal;
