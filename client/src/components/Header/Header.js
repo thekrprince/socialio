e75170f49e2d45227b50e66ad5325f28cc0b912e
Payal Sasmal <sasmalpayal@gmail.com>
2023-04-11 21:47:47 +0530

    fixed header css in mobile view

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,12 +1,11 @@
 import { useState } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import CreatePostModal from "../Modal/CreatePostModal";
-import { BsSearch,  } from "react-icons/bs";
+import { BsSearch } from "react-icons/bs";
 import { IoIosNotifications } from "react-icons/io";
 import "./Header.scss";
 import icon_girl from "../../assets/icons-female-user.svg";
 import logo from "../../assets/logo4.png";
-
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,9 +13,11 @@ const Header = () => {
   return (
     <>
       <div className="header_outer">
-        <img src={logo} alt="logo" className="logo" />
-        <input className="search" type="text" placeholder="Search..." />
-        <BsSearch className="search_icon" size={25} />
+        <div className="header_container">
+          <img src={logo} alt="logo" className="logo" />
+          <input className="search" type="text" placeholder="Search..." />
+          <BsSearch className="search_icon" size={25} />
+        </div>
         <div className="post_icon_manage">
           <button
             className="create_post_button"
@@ -24,13 +25,12 @@ const Header = () => {
           >
             Create Post
           </button>
-          <IoIosNotifications size={20} className="notification_icon"/>
+          <IoIosNotifications size={23} className="notification_icon" />
           <button className="profile_button">
             <img src={icon_girl} alt="profile" className="profile_icon" />
           </button>
           {/* <Link to="/">Logout</Link> */}
         </div>
-        
       </div>
       {isOpen && <CreatePostModal setIsOpen={setIsOpen} />}
     </>
