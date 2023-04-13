6e62778cc2c4777bb482242fb0a6041dffc21e41
Payal Sasmal <sasmalpayal@gmail.com>
2023-04-13 13:30:38 +0530

    fixed css for posts in mobile

--- client/src/components/posts/AllPosts.js
+++ client/src/components/posts/AllPosts.js
@@ -1,21 +1,66 @@
+import moment from "moment";
+import { RiDeleteBin6Fill } from "react-icons/ri";
 import "./AllPosts.scss";
 
-const AllPosts = ({allPostData }) => {
+const AllPosts = ({ allPostData }) => {
+
   return allPostData.map((post) => {
+  let date = new Date()
+  var dts = new Date(post.date);
+  var split_Values= dts.toLocaleString().split(" ");
+  var dates = split_Values[0];
+  var times = split_Values[1];
+  var then = dates + times
+  var ms = moment.utc(moment(date,"DD/MM/YYYY HH:mm:ss").diff(moment(then,"DD/MM/YYYY HH:mm:ss"))).format("HH:mm:ss");
+  var d = moment.duration(ms);
+  var timeAgo = d.humanize();
     return (
-      <div className="allPosts_outer">
-        <div className="allPosts_container">
+      <main>
+        <article className="post_article">
           <div>
-            <p>Name: {post.name}</p>
+            <div className="flex items-center ">
+              <div role="none" className="only_names">
+                <a className="name_details_outer" href=""></a>
+                <div className="name_details">
+                  <h3 className="font_details">{post.name}</h3>
+                </div>
+                <div className="time_dot">
+                  <span>.</span>
+                  {/* <p>
+                    <span> . </span>
+                    <span className="time_span"> {timeAgo}</span></p> */}
+                </div>
+                <div className="time">
+                  {timeAgo}
+                </div>
+              </div>
+              <div role="none">
+                <div><RiDeleteBin6Fill/></div>
+              </div>
+
+            </div>
+            <div className="mt-4">
+              <div className="article_content">
+
+              <p>{post.text}</p>
+              </div>
+            </div>
           </div>
-          <div>
-            <p>Text: {post.text}</p>
+        </article>
+        {/* <div className="allPosts_outer">
+          <div className="allPosts_container">
+            <div>
+              <p>{post.name}</p>
+            </div>
+            <div>
+              
+            </div>
+            <div>
+              <p>Date : {post.date}</p>
+            </div>
           </div>
-          <div>
-            <p>Date : {post.date}</p>
-          </div>
-        </div>
-      </div>
+        </div> */}
+      </main>
     );
   });
 };
