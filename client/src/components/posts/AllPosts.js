ccd9b8c5134515a83503cf982a801e90c9574f18
Payal Sasmal <sasmalpayal@gmail.com>
2023-04-13 18:15:22 +0530

    working on delete post

--- client/src/components/posts/AllPosts.js
+++ client/src/components/posts/AllPosts.js
@@ -2,9 +2,38 @@ import moment from "moment";
 import { RiDeleteBin6Fill } from "react-icons/ri";
 import { TfiCommentsSmiley } from "react-icons/tfi";
 import { AiOutlineHeart } from "react-icons/ai";
+import { baseURL } from "../../API";
 import "./AllPosts.scss";
+import { useEffect } from "react";
 
 const AllPosts = ({ allPostData }) => {
+  const [deletePost, setDeletePost] = ([]);
+
+  const token = sessionStorage.getItem("token");
+
+  const deleteHandler = (id) => {
+    console.log("id", id);
+    baseURL.delete(`/post/${id}`, Object.assign(
+      {},
+      {
+      headers: {
+        "X-Auth-Token": token,
+        "content-type": "application/json",
+        }
+      }
+     ) 
+    ).then((res) => {
+      // setDeletePost(deletePost.filter((postdel) => {
+      //   return postdel.id !== id;
+      // })
+        console.log(res);
+      }).catch((err) => {
+          console.log(err);
+      });
+  }
+
+  
+
   return allPostData.map((post) => {
     let date = new Date();
     var dts = new Date(post.date);
@@ -21,9 +50,10 @@ const AllPosts = ({ allPostData }) => {
       .format("HH:mm:ss");
     var d = moment.duration(ms);
     var timeAgo = d.humanize();
+
     return (
       <main>
-        <article className="post_article">
+        <article className="post_article" key={post._id}>
           <div>
             <div className="flex items-center ">
               <div role="none" className="only_names">
@@ -41,7 +71,7 @@ const AllPosts = ({ allPostData }) => {
               </div>
               <div role="none">
                 <div className="delete_icon">
-                  <RiDeleteBin6Fill />
+                  <RiDeleteBin6Fill onClick={deleteHandler(post._id)} />
                 </div>
               </div>
             </div>
