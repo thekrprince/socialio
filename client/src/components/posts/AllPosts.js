4a3b5fec3854ea63dc243b1b2c05a6659f49287d
Payal Sasmal <sasmalpayal@gmail.com>
2023-04-13 15:38:04 +0530

    fixed css for posts in mobile

--- client/src/components/posts/AllPosts.js
+++ client/src/components/posts/AllPosts.js
@@ -1,19 +1,26 @@
 import moment from "moment";
 import { RiDeleteBin6Fill } from "react-icons/ri";
+import { FcLike } from "react-icons/fc";
+import { TfiCommentsSmiley } from "react-icons/tfi";
 import "./AllPosts.scss";
 
 const AllPosts = ({ allPostData }) => {
-
   return allPostData.map((post) => {
-  let date = new Date()
-  var dts = new Date(post.date);
-  var split_Values= dts.toLocaleString().split(" ");
-  var dates = split_Values[0];
-  var times = split_Values[1];
-  var then = dates + times
-  var ms = moment.utc(moment(date,"DD/MM/YYYY HH:mm:ss").diff(moment(then,"DD/MM/YYYY HH:mm:ss"))).format("HH:mm:ss");
-  var d = moment.duration(ms);
-  var timeAgo = d.humanize();
+    let date = new Date();
+    var dts = new Date(post.date);
+    var split_Values = dts.toLocaleString().split(" ");
+    var dates = split_Values[0];
+    var times = split_Values[1];
+    var then = dates + times;
+    var ms = moment
+      .utc(
+        moment(date, "DD/MM/YYYY HH:mm:ss").diff(
+          moment(then, "DD/MM/YYYY HH:mm:ss")
+        )
+      )
+      .format("HH:mm:ss");
+    var d = moment.duration(ms);
+    var timeAgo = d.humanize();
     return (
       <main>
         <article className="post_article">
@@ -26,25 +33,31 @@ const AllPosts = ({ allPostData }) => {
                 </div>
                 <div className="time_dot">
                   <span className="span_time_dot">.</span>
-                  {/* <p>
-                    <span> . </span>
-                    <span className="time_span"> {timeAgo}</span></p> */}
                 </div>
                 <div className="time">
                   <time>{timeAgo}</time>
                 </div>
               </div>
               <div role="none">
-                <div><RiDeleteBin6Fill/></div>
+                <div>
+                  <RiDeleteBin6Fill />
+                </div>
               </div>
-
             </div>
-            <div className="mt-4">
+            <div className="article_outer">
               <div className="article_content">
-
-              <p>{post.text}</p>
+                <p>{post.text}</p>
               </div>
             </div>
+          </div>
+          <div className="interaction">
+            <div className="flex items-center ">
+              <div className="items">
+                <div className="like"><FcLike/></div>
+                <div className="comment"><TfiCommentsSmiley/></div>
+              </div>
+            </div>
+
           </div>
         </article>
         {/* <div className="allPosts_outer">
